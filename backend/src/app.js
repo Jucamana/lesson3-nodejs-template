41b686255f720e16daa24f8278d42d0a69912319
Juuso Kaarela <juusokaarela1@gmail.com>
2019-02-27 17:03:01 +0200

    uusi jutska

--- backend/src/app.js
+++ backend/src/app.js
@@ -30,10 +30,32 @@ const listChats = async (ctx) => {
   ctx.body = response;
 };
 
+const listChatsByRoom = async (ctx) => {
+  let room = ctx.params.room;
+  let options = {
+    where: {
+      room: room
+    }
+  };
+
+  let result = await database.Chat.findAll(options);
+  let chats = await Promise.all(result.map(chat => chat.toJSON()));
+
+  let response = {
+    results: chats,
+  };
+
+  ctx.body = response;
+};
+
 const createChat = async (ctx) => {
   const params = ctx.request.body;
+  let room = ctx.params.room;
 
-  const chat = await database.Chat.create({message: params.message});
+  const chat = await database.Chat.create({
+      message: params.message,
+      room: room,
+    });
 
   ctx.body = await chat.toJSON();
   ctx.status = 201;
@@ -44,7 +66,8 @@ const createChat = async (ctx) => {
 const publicRouter = new Router({ prefix: '/api' });
 
 publicRouter.get('/chats', listChats);
-publicRouter.post('/chats', createChat);
+publicRouter.get('/chats/:room', listChatsByRoom);
+publicRouter.post('/chats/:room', createChat);
 
 app.use(publicRouter.routes());
 app.use(publicRouter.allowedMethods());
